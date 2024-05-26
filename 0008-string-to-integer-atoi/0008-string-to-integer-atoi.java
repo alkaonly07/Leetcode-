448ad11c145d7ac90a922a0d9040b206ef58474e
Alka Kumari <107641479+alkaonly07@users.noreply.github.com>
2024-05-26 16:03:21 +0530

    Time: 1 ms (100.00%), Space: 41.8 MB (96.21%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,38 +1,27 @@
 class Solution {
     public int myAtoi(String s) {
-        int i = 0, n = s.length();
+        int i = 0;
+        int n = s.length();
         int sign = 1;
         int result = 0;
-
-        // Step 1: Ignore leading whitespaces
         while (i < n && s.charAt(i) == ' ') {
             i++;
         }
-
-        // Step 2: Check for the sign
         if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
             sign = (s.charAt(i) == '-') ? -1 : 1;
             i++;
         }
-
-        // Check if the next character after the sign is not a digit
         if (i < n && !Character.isDigit(s.charAt(i))) {
             return 0;
         }
-
-        // Step 3: Convert digits to an integer
         while (i < n && Character.isDigit(s.charAt(i))) {
             int digit = s.charAt(i) - '0';
-
-            // Step 4: Handle overflow and underflow
             if (result > (Integer.MAX_VALUE - digit) / 10) {
                 return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
-
             result = result * 10 + digit;
             i++;
         }
-
         return result * sign;
     }
 }
