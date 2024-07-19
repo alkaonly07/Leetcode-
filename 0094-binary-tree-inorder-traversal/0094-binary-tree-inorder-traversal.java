1e74630c3f3e869718ddb670783a2ee6214c2618
Alka Kumari <107641479+alkaonly07@users.noreply.github.com>
2024-07-19 09:47:22 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (20.35%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -20,11 +20,22 @@ class Solution {
         return result;
     }
     public void inOrderHelper(TreeNode node, List<Integer> result){
-        if(node==null){
-            return;
+        // if(node==null){
+        //     return;
+        // }
+        // inOrderHelper(node.left, result);
+        // result.add(node.val);
+        // inOrderHelper(node.right,result);
+        Stack<TreeNode> stack= new Stack<>();
+        TreeNode current = node;
+        while(current!=null || !stack.isEmpty()){
+            while(current!=null){
+                stack.push(current);
+                current=current.left;
+            }
+            current=stack.pop();
+            result.add(current.val);
+            current=current.right;
         }
-        inOrderHelper(node.left, result);
-        result.add(node.val);
-        inOrderHelper(node.right,result);
     }
 }
