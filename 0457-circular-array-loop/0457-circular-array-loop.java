ffa26af286eefee0b6700461d40fd296368b9954
Alka Kumari <107641479+alkaonly07@users.noreply.github.com>
2024-09-18 16:28:26 +0530

    Time: 15 ms (45.12%), Space: 40.8 MB (77.06%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.java
+++ 0457-circular-array-loop/0457-circular-array-loop.java
@@ -1,58 +1,49 @@
 class Solution {
     public boolean circularArrayLoop(int[] nums) {
-        if(nums.length <= 1) {
-      return false;
-    }
-    
-    for(int i = 0; i < nums.length; i++) {
-      
-      int slow, fast;
-      slow = fast = i;
-      boolean ifForward = nums[i] > 0;
-      
-      while(true) {
-        slow = getNextPosition(nums, slow, ifForward);
-        if(slow == -1) {
-          break;
+        if(nums.length<=1){
+            return false;
         }
-        
-        fast = getNextPosition(nums, fast, ifForward);
-        if(fast == -1) {
-          break;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int slow = i;
+            int fast = i;
+            boolean ifForward = nums[i]>0;
+            while(true){
+                slow = getNextPosition(nums, slow, ifForward);
+                if(slow == -1){
+                    break;
+                }
+                fast = getNextPosition(nums, fast, ifForward);
+                if(fast == -1){
+                    break;
+                }
+                fast = getNextPosition(nums, fast, ifForward);
+                if(fast == -1){
+                    break;
+                }
+                if(slow == fast){
+                    return true;
+                }
+                
+            }
         }
+        return false;
         
-        fast = getNextPosition(nums, fast, ifForward);
-        if(fast == -1) {
-          break;
+    }
+    private int getNextPosition(int[] nums, int index, boolean ifForward){
+        boolean direction = nums[index]>=0;
+        if(direction != ifForward){
+            return -1;
         }
+        int nextIndex = (index + nums[index]) % nums.length;
         
-        if(slow == fast) {
-          return true;
-        }  
-      }
-    }
-    
-    return false;
-  }
-  
-  private static int getNextPosition(int[] nums, int index, boolean ifForward) {
-    boolean direction = nums[index] >= 0;
-    
-    if(direction != ifForward) {
-      return -1;
-    }
-    
-    int nextIndex = (index + nums[index]) % nums.length;
-    
-    if(nextIndex < 0) {
-      nextIndex = nextIndex + nums.length;
-    }
-    
-    if(nextIndex == index) {
-      return -1;
-    }
-    
-    return nextIndex;
+        if(nextIndex < 0){
+            nextIndex = nextIndex + nums.length;
+        }
+        if(nextIndex == index){
+            return -1;
+        }
+        return nextIndex;
         
     }
 }
