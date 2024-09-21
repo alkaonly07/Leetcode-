ae9add5b25e0b8f86427878fc570c1cec75ba3f0
Alka Kumari <107641479+alkaonly07@users.noreply.github.com>
2024-09-21 15:20:08 +0530

    Time: 0 ms (100.00%), Space: 41 MB (70.81%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -10,32 +10,23 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        
-    if (head == null || left == right) {
-            return head;  // No need to reverse if head is null or left == right
+        if(head == null || left == right){
+            return head;
         }
-        
-        // Step 1: Move to the node before `left` position
-        ListNode dummy = new ListNode(0); // Use a dummy node to simplify edge cases
-        dummy.next = head;
+        ListNode dummy = new ListNode(0);
+        dummy.next=head;
         ListNode prev = dummy;
-        
-        for (int i = 1; i < left; i++) {
-            prev = prev.next;  // Move `prev` to the node before the `left` node
+        for(int i = 1; i<left ; i++){
+            prev=prev.next;
         }
-        
-        // Step 2: Reverse the sublist between `left` and `right`
-        ListNode start = prev.next;   // `start` is the first node of the sublist to reverse
-        ListNode then = start.next;   // `then` is the node that will be reversed
-        
-        // Perform the reversal of the sublist
-        for (int i = 0; i < right - left; i++) {
-            start.next = then.next;   // Move `then` node after the `start` node
-            then.next = prev.next;    // Move `then` node to the front of the sublist
-            prev.next = then;         // Update the `prev.next` to point to the `then` node
-            then = start.next;        // Move `then` to the next node to reverse
+        ListNode start = prev.next;
+        ListNode then = start.next;
+        for(int i = 0; i<right-left;i++){
+            start.next = then.next;
+            then.next = prev.next;
+            prev.next = then;
+            then=start.next;
         }
-        
         return dummy.next;
     }
 }
