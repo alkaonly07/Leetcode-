859b5c39032ef39f51134a0cc2f7fd30b1170717
Alka Kumari <107641479+alkaonly07@users.noreply.github.com>
2024-10-14 16:48:33 +0530

    Time: 152 ms (58.77%), Space: 63.4 MB (15.64%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.java
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.java
@@ -5,23 +5,12 @@ class Solution {
         for(int i : nums){
             maxHeap.add(i);
         }
-        while(k-- > 0){
-            int maxElement = maxHeap.poll();
-            score += maxElement;
-            maxHeap.add((int)Math.ceil(maxElement / 3.0));
-        
+        while(k-- >0){
+            int element = maxHeap.poll();
+            score += element;
+            maxHeap.add((int)Math.ceil(element/3.0));
         }
         return score;
         
     }
-    // private int ceil(int num, int x){
-    //     int div = num/x ;
-    //     if(num%x != 0){
-    //         div++;
-    //     }
-    //     return div;
-    // }
-    // private void findingMax(int[] nums){
-    //     Arrays.sort(nums);
-    // }
 }
