6a2036c1ba906c460310cbf2d84d687be986e901
Alka Kumari <107641479+alkaonly07@users.noreply.github.com>
2024-10-16 12:18:58 +0530

    Time: 2 ms (85.76%), Space: 45.7 MB (80.32%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int  first = 0;
+        for(int second = 0;second<nums.length; second++){
+            if(nums[second] != 0){
+                int temp= nums[first];
+                nums[first] = nums[second];
+                nums[second]= temp;
+                first++;
+            }
+        }
+    }
+    
+}
