9607a97e1b87871fe1887400c5155c4126cd0337
Alka Kumari <107641479+alkaonly07@users.noreply.github.com>
2024-10-24 00:31:32 +0530

    Time: 1 ms (98.69%), Space: 44.8 MB (75.58%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,30 +1,23 @@
 class Solution {
     public int jump(int[] nums) {
-       int farthest = 0;  // Track the farthest point we can reach
-        int currentEnd = 0;  // The end of the current jump
-        int jumps = 0;  // Number of jumps made
-
-        if (nums.length <= 1) {
-            return 0;  // No jumps needed if the array has 1 or fewer elements
+        int farthest = 0;
+        int current = 0;
+        int jump = 0;
+        if(nums.length <=1){
+            return 0;
         }
-
-        // Iterate through the array but exclude the last element
-        for (int i = 0; i < nums.length - 1; i++) {
-            farthest = Math.max(farthest, i + nums[i]);  // Update the farthest point we can reach
-
-            // If we reach the end of the current jump range
-            if (i == currentEnd) {
-                jumps++;  // Make a jump
-                currentEnd = farthest;  // Update the end of the current jump
-
-                // If the farthest point reaches or exceeds the last index, return the number of jumps
-                if (farthest >= nums.length - 1) {
-                    return jumps;
+        for(int i=0;i<nums.length;i++){
+            farthest = Math.max(farthest,i+nums[i]);
+            if(i == current){
+                jump++;
+                current = farthest;
+                if(farthest >= nums.length-1){
+                    return jump;
                 }
             }
+            
         }
-        return 0;  
+        return 0;
         
     }
-    }
-                     
+}
