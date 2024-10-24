f7e1d69c911c6d87c04c9fb4c11d8caa3df513a4
Alka Kumari <107641479+alkaonly07@users.noreply.github.com>
2024-10-24 23:56:48 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (11.84%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,12 +3,12 @@ class Solution {
         if(n==1){
             return 1;
         }
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i = 3; i<=n;i++){
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i = 2; i<n;i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
-        return dp[n];
+        return dp[n-1];
     }
 }
