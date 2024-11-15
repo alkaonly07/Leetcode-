dd7b670b98b034a7f028e9f31afa217d62d0cdf2
Alka Kumari <107641479+alkaonly07@users.noreply.github.com>
2024-11-15 18:17:12 +0530

    Time: 1 ms, Memory: 41.2 MB - LeetHub

---
+++ reverse-integer/reverse-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+        int newX = 0;
+        while(x!=0){
+            int y=x%10;
+            x=x/10;
+            if(newX > Integer.MAX_VALUE/10 || (newX == Integer.MAX_VALUE/10 &&  y>7)){
+                return 0;
+            }
+            if (newX < Integer.MIN_VALUE / 10 || (newX == Integer.MIN_VALUE / 10 && y < -8)) {
+                return 0; // Underflow
+            }
+            newX= newX*10+ y ;
+        }
+        
+        return newX;
+        
+    }
+}
