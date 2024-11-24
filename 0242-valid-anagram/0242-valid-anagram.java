3334a906e6c71e42ac51372527241e25d26e536a
Alka Kumari <107641479+alkaonly07@users.noreply.github.com>
2024-11-24 13:34:25 +0530

    Time: 6 ms (44.26%), Space: 43 MB (76.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,13 +3,14 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        int[] charCount = new int[26];
-        for(int i=0;i<s.length();i++){
-            charCount[s.charAt(i)-'a']++;
-            charCount[t.charAt(i)-'a']--;
+        int[] nums = new int[26];
+        
+        for(int i = 0;i<s.length();i++){
+            nums[s.charAt(i) - 'a']++;
+             nums[t.charAt(i) - 'a']--;   
         }
-        for(int count : charCount){
-            if(count != 0){
+        for(int n: nums){
+            if(n != 0){
                 return false;
             }
         }
