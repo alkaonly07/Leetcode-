5c2a5712313e70127341afee1d196fb5a5583690
Alka Kumari <107641479+alkaonly07@users.noreply.github.com>
2024-12-09 12:04:52 +0530

    Time: 15 ms (26.14%), Space: 48.3 MB (19.01%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,17 +3,36 @@ class Solution {
         if(strs == null || strs.length==0){
             return new ArrayList<>();
         }
-        HashMap<String, List<String>> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
         for(String s : strs){
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            String key = String.valueOf(arr);
-            if(!map.containsKey(key)){
-                map.put(key, new ArrayList<>());
+            String key = getFrequency(s);
+            if(map.containsKey(key)){
+                map.get(key).add(s);
             }
-            map.get(key).add(s);
+            else{
+                List<String> s1 = new ArrayList<>();
+                s1.add(s);
+                map.put(key, s1);
+            }
+           
         }
-        return new ArrayList<>(map.values());
+    
+         return new ArrayList<>(map.values());
+    }
         
+    
+    private String getFrequency(String str){
+        int[] freq = new int[26];
+        for(char ch : str.toCharArray()){
+            freq[ch - 'a']++;
+        }
+        StringBuilder frequencyString = new StringBuilder("");
+        char c = 'a';
+        for(int i : freq){
+            frequencyString.append(c);
+            frequencyString.append(i);
+            c++;
+        }
+        return frequencyString.toString();
     }
 }
