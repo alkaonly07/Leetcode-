5487b428c8a44c53a2673b3143ac8133ae838f5b
Alka Kumari <107641479+alkaonly07@users.noreply.github.com>
2024-12-23 21:29:57 +0530

    Time: 2187 ms (5.07%), Space: 69.7 MB (6.99%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,13 +1,50 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> maxPq = new PriorityQueue<>((a,b) -> (b-a));
-        for(int num : nums){
-            maxPq.add(num);
+         int n = nums.length;
+        // Convert kth largest to index in sorted array
+        int targetIndex = n - k;
+        return quickSelect(nums, 0, n - 1, targetIndex);
+    }
+
+    private int quickSelect(int[] nums, int left, int right, int targetIndex) {
+        // Choose a random pivot to improve performance
+        int pivotIndex = new Random().nextInt(right - left + 1) + left;
+        int partitionIndex = partition(nums, left, right, pivotIndex);
+
+        // If partition index matches the target index, return the value
+        if (partitionIndex == targetIndex) {
+            return nums[partitionIndex];
+        } else if (partitionIndex < targetIndex) {
+            // Search in the right part
+            return quickSelect(nums, partitionIndex + 1, right, targetIndex);
+        } else {
+            // Search in the left part
+            return quickSelect(nums, left, partitionIndex - 1, targetIndex);
         }
-        for(int i =1;i<k;i++){
-            maxPq.poll();
+    }
+
+    private int partition(int[] nums, int left, int right, int pivotIndex) {
+        int pivotValue = nums[pivotIndex];
+        // Move pivot to the end
+        swap(nums, pivotIndex, right);
+        int storeIndex = left;
+
+        // Move all smaller elements to the left
+        for (int i = left; i < right; i++) {
+            if (nums[i] < pivotValue) {
+                swap(nums, i, storeIndex);
+                storeIndex++;
+            }
         }
-        return maxPq.peek();
-        
+
+        // Move pivot to its final place
+        swap(nums, storeIndex, right);
+        return storeIndex;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
